6a60db67701ba76be9e66724b790a0e85cdc75a9
LIWEI2415 <3151727911@qq.com>
2021-04-22 09:53:50 +0800

    小小的修改

--- Java/LWTest1/src/com/lw/work/graduation_project.java
+++ Java/LWTest1/src/com/lw/work/graduation_project.java
@@ -9,11 +9,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class graduation_project {
-    String fileURL="LWTest1\\src\\com\\lw\\material\\wallRevit.ifc";//文件所在地
+
+    String fileURL="LWTest1\\src\\com\\lw\\material\\wallRevit.ifc";//文件所在相对位置
     String usefulStart="DATA;";//有用的开始
     int lengthOfStart=7;//indexOf只能取到“D”的位置，要往后补位置
     String usefulEnd="ENDSEC;";//有用的结束
-    String jsonPath="D:\\a学习资料\\毕设\\demo\\html_json\\FirstJson.json";
+    String jsonPath="D:\\a学习资料\\毕设\\demo\\html_json\\FirstJson.json";//存放json文件的绝对位置
+
     public static void main(String[] args){
 
         graduation_project project=new graduation_project ();
